d6736ef6eddca0927cd5bce475479a238f91e3b0
Amazia Gur <amaziagur@Amazias-MacBook-Pro.local>
2025-03-30 20:34:47 +0100

    chore: clean up e2e

--- tests/test_e2e_ophelos_rating.py
+++ tests/test_e2e_ophelos_rating.py
@@ -11,22 +11,31 @@ from service.schemas.statement_schema import StatementRequest
 from service.statements.statement_service import USER_NOT_FOUND, STATEMENT_NOT_FOUND
 from service.users.user_service import UserService
 
+INVALID_STATEMENT_ID = 999
+
+VALID_STATEMENT_ID = 1
+
+INVALID_USER_ID = 999
+FIRST_VALID_USER_ID = 1
+SECOND_VALID_USER_ID = 2
+
 
 def test_is_healthy(app):
     assert app.is_healthy()
 
 
 def test_submit_new_statement(app):
-    response = app.submit_statement(build_statement(1))
+    response = app.submit_statement(build_statement(FIRST_VALID_USER_ID))
     is_not(response["statement_id"], none())
 
 
 def test_retrieve_statement(app):
-    response = app.submit_statement(build_statement(1))
+    response = app.submit_statement(build_statement(FIRST_VALID_USER_ID))
     statement_id = response["statement_id"]
-    report = app.get_statement(statement_id, 1)
+    report = app.get_statement(statement_id, FIRST_VALID_USER_ID)
+
     assert_that(report["id"], equal_to(statement_id))
-    assert_that(report["user_id"], equal_to(1))
+    assert_that(report["user_id"], equal_to(FIRST_VALID_USER_ID))
     assert_that(report["incomes"], has_length(1))
     assert_that(report["incomes"][0]["category"], equal_to("Salary"))
     assert_that(report["incomes"][0]["amount"], equal_to(5000.0))
@@ -36,20 +45,20 @@ def test_retrieve_statement(app):
 
 
 def test_unable_to_retrieve_statement_of_different_user(app):
-    response = app.submit_statement(build_statement(1))
+    response = app.submit_statement(build_statement(FIRST_VALID_USER_ID))
     statement_id = response["statement_id"]
 
     with pytest.raises(requests.HTTPError) as exc_info:
-        app.get_statement(statement_id, 2)
+        app.get_statement(statement_id, SECOND_VALID_USER_ID)
 
     assert_that(exc_info.value.response.status_code, is_(404))
 
 
 def test_calculate_ie_rating_success(app):
-    response = app.submit_statement(build_statement(1))
+    response = app.submit_statement(build_statement(FIRST_VALID_USER_ID))
     statement_id = response["statement_id"]
 
-    rating_response = app.get_rating(statement_id, 1)
+    rating_response = app.get_rating(statement_id, FIRST_VALID_USER_ID)
 
     assert_that(rating_response["total_income"], equal_to(5000.0))
     assert_that(rating_response["total_expenditure"], equal_to(1500.0))
@@ -59,7 +68,7 @@ def test_calculate_ie_rating_success(app):
 
 def test_calculate_ie_rating_user_not_found(app):
     with pytest.raises(requests.HTTPError) as exc_info:
-        app.get_rating(1, 999)
+        app.get_rating(VALID_STATEMENT_ID, INVALID_USER_ID)
 
     assert_that(exc_info.value.response.status_code, is_(404))
     assert_that(exc_info.value.response.json()["detail"], equal_to(USER_NOT_FOUND))
@@ -67,7 +76,7 @@ def test_calculate_ie_rating_user_not_found(app):
 
 def test_calculate_ie_rating_statement_not_found(app):
     with pytest.raises(requests.HTTPError) as exc_info:
-        app.get_rating(999, 1)
+        app.get_rating(INVALID_STATEMENT_ID, FIRST_VALID_USER_ID)
     assert_that(exc_info.value.response.status_code, is_(404))
     assert_that(exc_info.value.response.json()["detail"], equal_to(STATEMENT_NOT_FOUND))
 
@@ -75,10 +84,11 @@ def test_calculate_ie_rating_statement_not_found(app):
 def test_calculate_period_rating(app):
     clean_db()
 
-    app.submit_statement(build_statement(1))
-    app.submit_statement(build_statement(1))
+    app.submit_statement(build_statement(FIRST_VALID_USER_ID))
+    app.submit_statement(build_statement(FIRST_VALID_USER_ID))
 
-    response = app.get_rating_period(1, start_date="2025-01-01", end_date="2025-12-31")
+    response = app.get_rating_period(FIRST_VALID_USER_ID,
+                                     start_date="2025-01-01", end_date="2025-12-31")
 
     assert_that(response["total_income"], equal_to(10000.0))
     assert_that(response["total_expenditure"], equal_to(3000.0))
